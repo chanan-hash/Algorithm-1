304503cacde2fc7e0f64745081fa920188a52048
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-07-21 10:45:36 +0300

    starting building the algorithm

--- Gale Shaply - Stable matching/main.java
+++ Gale Shaply - Stable matching/main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * This classes is trying to implement the Stable-Marriage Algorithm of Gale-Shapley.
  * This is according to open-university course
@@ -16,14 +18,30 @@ public class main {
 //    {0, 1, 2, 3},
 //    {0, 1, 2, 3}};
 
-        int[][] womanPref = new int[5][5];
+        int[][] womanPref = {{2,3,1,5,4},
+                             {5,2,1,4,3},
+                            {3,4,2,1,5},
+                            {4,2,5,3,1},
+                            {2,5,4,1,3}
+                                       };
         int[][] womenRank = new int[5][5];
-
+        System.out.println(Arrays.deepToString(womanPref));
+        translateToRank(womanPref,womenRank);
+        System.out.println();
+        System.out.println(Arrays.deepToString(womenRank));
     }
 
     // For accessing the men rank in the women setion, we need to translate it to comfortable workspace
+    //    for(j=1 to k,i=1 to k)
+    //      h=WomanPref[j,i]
+    //      WomanRank[j,h]=i
     public static void translateToRank(int[][] womanPref, int[][] womenRank) {
-
+        for (int i = 0; i <= womenRank.length; i++) {
+            for (int j = 0; j <= womenRank[0].length; j++) {
+                int h = womanPref[i][j];
+                womenRank[i][h] = j; // hte h becoming 5, and there is no index of it
+            }
+        }
     }
 
     public static int[][] stableMarriage(int prefer[][]) {
