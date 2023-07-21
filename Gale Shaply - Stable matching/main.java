bb272549f0ed9e081a5879fd06abcf0d8c01a168
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-07-21 10:24:00 +0300

    updating

---
+++ Gale Shaply - Stable matching/main.java
@@ -0,0 +1,33 @@
+/**
+ * This classes is trying to implement the Stable-Marriage Algorithm of Gale-Shapley.
+ * This is according to open-university course
+ * https://courses.campus.gov.il/courses/course-v1:OUI+ACD_RFP4_Algorithm_HE+2022_1/courseware/a500288cec0348bdbcabd807367da7c4/65068c9a169c4dfe854634dc448ec90a/
+ */
+
+public class main {
+    public static void main(String[] args) {
+        int[][] manPref = new int[5][5];
+//        7, 5, 6, 4},
+//    {5, 4, 6, 7},
+//    {4, 5, 6, 7},
+//    {4, 5, 6, 7},
+//    {0, 1, 2, 3},
+//    {0, 1, 2, 3},
+//    {0, 1, 2, 3},
+//    {0, 1, 2, 3}};
+
+        int[][] womanPref = new int[5][5];
+        int[][] womenRank = new int[5][5];
+
+    }
+
+    // For accessing the men rank in the women setion, we need to translate it to comfortable workspace
+    public static void translateToRank(int[][] womanPref, int[][] womenRank) {
+
+    }
+
+    public static int[][] stableMarriage(int prefer[][]) {
+
+        return null;
+    }
+}
