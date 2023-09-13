29a0326a5c7951454326759f1a648751b9933121
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-09-13 10:09:38 +0300

    "Adding dynamic programing for subset sum"

--- Excr Algorithms/SubsetSum.java
+++ Excr Algorithms/SubsetSum.java
@@ -21,4 +21,69 @@ public class SubsetSum {
         }
         return subsetSum(arr, n - 1, sum) || subsetSum(arr, n - 1, sum - arr[n - 1]);
     }
+
+    /**
+     *  From geeksforgeeks:
+     * https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
+     * Time Complexity O(sum * n) where n is the size of the array.
+     * Auxiliary Space: O(sum), as the size of the 1-D array is sum+1.
+     */
+
+    /*
+    // A Dynamic Programming solution for subset
+// sum problem
+import java.io.*;
+class GFG {
+
+    // Returns true if there is a subset of
+    // set[] with sum equal to given sum
+    static boolean isSubsetSum(int set[], int n, int sum)
+    {
+        // The value of subset[i][j] will be
+        // true if there is a subset of
+        // set[0..j-1] with sum equal to i
+        boolean subset[][] = new boolean[sum + 1][n + 1];
+
+        // If sum is 0, then answer is true
+        for (int i = 0; i <= n; i++)
+            subset[0][i] = true;
+
+        // If sum is not 0 and set is empty,
+        // then answer is false
+        for (int i = 1; i <= sum; i++)
+            subset[i][0] = false;
+
+        // Fill the subset table in bottom
+        // up manner
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 1; j <= n; j++) {
+                subset[i][j] = subset[i][j - 1];
+                if (i >= set[j - 1])
+                    subset[i][j]
+                        = subset[i][j]
+                          || subset[i - set[j - 1]][j - 1];
+            }
+        }
+
+        return subset[sum][n];
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        int set[] = { 3, 34, 4, 12, 5, 2 };
+        int sum = 9;
+        int n = set.length;
+        if (isSubsetSum(set, n, sum) == true)
+            System.out.println("Found a subset"
+                               + " with given sum");
+        else
+            System.out.println("No subset with"
+                               + " given sum");
+    }
+}
+
+/* This code is contributed by Rajat Mishra */
+
+
 }
